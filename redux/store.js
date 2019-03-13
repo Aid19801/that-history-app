dc8f6801462c6b1befad58851d71403989d45cde
Adrian Thompson <adrian.thompson@infabode.com>
2019-03-13 10:31:26 +0800

    redux dev tools working

--- redux/store.js
+++ redux/store.js
@@ -1,11 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 import RootReducer from './reducers';
 import RootSaga from './sagas'
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(RootReducer, applyMiddleware(sagaMiddleware));
+const store = createStore(RootReducer, composeWithDevTools(
+                applyMiddleware(sagaMiddleware),
+  ));
 
 sagaMiddleware.run(RootSaga);
 
